7a9be43c25cb618f1de60990f479ca521c495fde
Volodymyr Vlasiuk <trivvet@gmail.com>
2018-08-05 13:12:17 +0300

    improve detail page

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,14 +3,45 @@
 angular.module('blogDetail').
     component('blogDetail', {
         templateUrl: '/templates/blog-detail.html',
-        controller: function($routeParams, $scope) {
+        controller: function($location, $routeParams, $scope) {
             console.log("blog detail");
-            var blogTitle = "Blog " + $routeParams.id;
-            var blogInfo = {
-                title: blogTitle,
-                description: "Here must be an info about post"
-            }
+            var blogItems = [
+                {
+                    id: 1,
+                    title: "First Blog",
+                    description: "It's first blog in this site",
+                    publishedDate: "20-12-2016"
+                },
+                {
+                    id: 2,
+                    title: "Second Blog",
+                    description: "It's about some people",
+                    publishedDate: "22-12-2016"
+                },
+                {
+                    id: 3,
+                    title: "Third Blog",
+                    description: "Only for testing",
+                    publishedDate: "25-12-2016"
+                },
+                {
+                    id: 4,
+                    title: "Fourth Blog",
+                    description: "Clear information",
+                    publishedDate: "01-01-2017"
+                }
 
-            $scope.post = blogInfo;
+            ];
+            $scope.notFound = true;
+            blogItems.forEach(function(post) {
+                if (post.id == $routeParams.id) {
+                    $scope.post = post;
+                    $scope.notFound = false;
+                }
+            });
+
+            if ($scope.notFound) {
+                $location.path("/404");
+            }
         }
     });
