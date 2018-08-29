daa1508dc113b792a76d98137f29b4941636119b
Volodymyr Vlasiuk <trivvet@gmail.com>
2018-08-29 16:20:40 +0300

    end first version of confirm click

--- src/js/app/utils/confirm-click/confirm-click.directive.js
+++ src/js/app/utils/confirm-click/confirm-click.directive.js
@@ -1,5 +1,21 @@
 'use strict';
 
+angular.module('confirmClick').
+    directive('confirmClick', function() {
+        return {
+            restrict: "A",
+            link: function (scope, element, attr) {
+                var msg = attr.confirmClick || "Are you sure?";
+                var clickAction = attr.confirmedClick;
+                element.bind('click', function (event) {
+                    if ( window.confirm(msg) ) {
+                        scope.$eval(clickAction);
+                    }
+                });
+            }
+        }
+    });
+
 // angular.module('confirmClick').
 //     directive('confirmClick', function($rootScope, $location){
 //         return {
