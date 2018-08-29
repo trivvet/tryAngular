c3df269656c235605431ee82dc478fe252785fdd
Volodymyr Vlasiuk <trivvet@gmail.com>
2018-08-29 16:03:13 +0300

    end demo confirm click directive

--- src/js/app/utils/confirm-click/confirm-click.directive.js
+++ src/js/app/utils/confirm-click/confirm-click.directive.js
@@ -1,21 +1,23 @@
 'use strict';
 
-angular.module('confirmClick').
-    directive('confirmClick', function(){
-        return {
-            scope: {
-                message: '@message',
-                post: '=post',
-            },
-            restrict: "E",
-            template: "<a ng-href='/blog/{{ post.id }}'>{{ post.title }}</a>",
-            link: function(scope, element, attr){
-                console.log('scope');
-                console.log(scope.message);
-                console.log(scope.post);
-                console.log('element: ' + element);
-                console.log('attr');
-                console.log(attr.confirmClick);
-            }
-        }
-    });
+// angular.module('confirmClick').
+//     directive('confirmClick', function($rootScope, $location){
+//         return {
+//             scope: {
+//                 message: '@message',
+//                 post: '=post',
+//             },
+//             restrict: "E",
+//             template: "<a ng-href='#'>{{ post.title }}</a>",
+//             link: function(scope, element, attr){
+//                 var msg = scope.message || "Are you sure?";
+//                 element.bind("click", function(event){
+//                     if (window.confirm(msg)) {
+//                         $rootScope.$apply(function() {
+//                             $location.path('/blog/' + scope.post.id);    
+//                         });
+//                     }    
+//                 });
+//             }
+//         }
+//     });
